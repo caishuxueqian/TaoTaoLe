3b58b3cd32a23436b108002e713e0c3a4cc45c15
thehsy <thehsys@gmail.com>
2019-05-30 16:31:18 +0800

    界面美化

--- app/src/main/java/orc/com/taotaole/Activity/ForgetActivity.java
+++ app/src/main/java/orc/com/taotaole/Activity/ForgetActivity.java
@@ -1,7 +1,11 @@
 package orc.com.taotaole.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import orc.com.taotaole.R;
 
@@ -11,5 +15,14 @@ public class ForgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
+        Button button = findViewById(R.id.btn_forget);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetActivity.this,LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(ForgetActivity.this, "密码重置成功！", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/orc/com/taotaole/Activity/RegisterActivity.java
+++ app/src/main/java/orc/com/taotaole/Activity/RegisterActivity.java
@@ -1,7 +1,11 @@
 package orc.com.taotaole.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import orc.com.taotaole.R;
 
@@ -11,5 +15,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button button = findViewById(R.id.btn_register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(RegisterActivity.this, "注册成功！", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
