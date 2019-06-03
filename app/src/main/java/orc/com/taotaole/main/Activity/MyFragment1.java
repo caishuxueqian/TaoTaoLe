03fcf0c18c1de785c01f2a2a84653496d65d46e7
thehsy <thehsys@gmail.com>
2019-06-03 17:06:00 +0800

    主页面的实现

---
+++ app/src/main/java/orc/com/taotaole/main/Activity/MyFragment1.java
@@ -0,0 +1,26 @@
+package orc.com.taotaole.main.Activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import orc.com.taotaole.R;
+
+public class MyFragment1 extends Fragment {
+
+    public MyFragment1() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_content, container, false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText("第一个Fragment");
+        Log.e("HEHE", "1日狗");
+        return view;
+    }
+}
